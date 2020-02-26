f5ae557f1314d7185a26dfb7fc761e56978a056f
mr-ahir-is-here <61195233+mr-ahir-is-here@users.noreply.github.com>
2020-02-26 16:37:43 +0530

    Delete main - Copy.js

--- main - Copy.js
+++
@@ -1,8 +0,0 @@
-function generateQRCode() {
-	var data = document.getElementById("data").value
-	eel.generate_qr(data)(setImage)
-}
-
-function setImage(base64) {
-	document.getElementById("qr").src = base64
-}
